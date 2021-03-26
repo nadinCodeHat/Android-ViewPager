803320389926238a7d1ae355306f1678bf06d020
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2021-03-26 10:45:48 +0530

    Updated IntroFragement.java

--- app/src/main/java/com/example/androidviewpager/IntroFragment.java
+++ app/src/main/java/com/example/androidviewpager/IntroFragment.java
@@ -1,14 +1,55 @@
 package com.example.androidviewpager;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.fragment.app.Fragment;
+
+public class IntroFragment extends Fragment {
+    private static final String BACKGROUND_COLOR = "backgroundColor";
+    private static final String PAGE = "page";
+    private int mBackgroundColor, mPage;
+
+    public static IntroFragment newInstance(int backgroundColor, int page) {
+        IntroFragment frag = new IntroFragment();
+        Bundle b = new Bundle();
+        b.putInt(BACKGROUND_COLOR, backgroundColor);
+        b.putInt(PAGE, page);
+        frag.setArguments(b);
+        return frag;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBackgroundColor = getArguments().getInt(BACKGROUND_COLOR);
+        mPage = getArguments().getInt(PAGE);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        View background = view.findViewById(R.id.intro_background);
+        background.setBackgroundColor(mBackgroundColor);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        int layoutResId;
+        switch (mPage) {
+            case 0:
+                layoutResId = R.layout.intro_fragment_layout_1; break;
+            case 1:
+                layoutResId = R.layout.intro_fragment_layout_2; break;
+            case 2:
+                layoutResId = R.layout.intro_fragment_layout_3; break;
+            default:
+                layoutResId = R.layout.intro_fragment_layout_4; break;
+        }
+        View view = getActivity().getLayoutInflater().inflate(layoutResId, container, false);
+        view.setTag(mPage);
+        return view;
     }
 }
